d3be7e3a8e5016b911872dd7c3cadbc371f2e4fa
maricoIR <matin.kahrizi@gmail.com>
2022-07-06 15:51:20 +0430

    Main App Config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        theme="colored"
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        draggable={false}
+        pauseOnVisibilityChange
+        closeOnClick
+        pauseOnHover
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
