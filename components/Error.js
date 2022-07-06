cb71962c88fdda9ba84ca7a9920017471d0462fd
maricoIR <matin.kahrizi@gmail.com>
2022-07-06 15:52:29 +0430

    Error Component

---
+++ components/Error.js
@@ -0,0 +1,15 @@
+const Error = () => {
+  return (
+    <div className="w-full flex flex-col gap-5 items-center justify-center mt-12 px-5">
+      <div className="md:w-[800px] w-full rounded-md py-3 px-1 shadow-sm border border-darkborder bg-dark flex flex-col items-start gap-2">
+        <div className="w-full flex items-center justify-center py-3 px-5">
+          <span className="font-medium text-xl text-red-500">
+            Some Error occurred !
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
