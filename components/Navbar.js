a1fb92e06d46e2f05833b417747d3b5709e50431
maricoIR <matin.kahrizi@gmail.com>
2022-07-20 17:12:12 +0430

    contact links fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,24 +5,22 @@ const Navbar = () => {
     <div className="bg-dark shadow-sm w-full py-5 md:px-8 px-5 flex items-center justify-between">
       <span className="font-medium text-xl text-white">Next Todo App</span>
       <div className="flex items-center gap-5">
-        <LogoGithub
-          color={"#fff"}
-          width="23px"
-          height="23px"
-          cssClasses="cursor-pointer"
-        />
-        <LogoLinkedin
-          color={"#fff"}
-          width="23px"
-          height="23px"
-          cssClasses="cursor-pointer"
-        />
-        <GlobeOutline
-          color={"#fff"}
-          width="23px"
-          height="23px"
-          cssClasses="cursor-pointer"
-        />
+        <a href="https://github.com/maricoIR" target="_blank">
+          <LogoGithub
+            color={"#fff"}
+            width="23px"
+            height="23px"
+            cssClasses="cursor-pointer"
+          />
+        </a>
+        <a href="https://matinkahrizi.vercel.app/" target="_blank">
+          <GlobeOutline
+            color={"#fff"}
+            width="23px"
+            height="23px"
+            cssClasses="cursor-pointer"
+          />
+        </a>
       </div>
     </div>
   );
