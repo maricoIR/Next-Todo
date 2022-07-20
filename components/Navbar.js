cbea7dd22678f97da14b86d61616bb48abe7470a
maricoIR <matin.kahrizi@gmail.com>
2022-07-20 22:18:02 +0430

    noreffer links

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="bg-dark shadow-sm w-full py-5 md:px-8 px-5 flex items-center justify-between">
       <span className="font-medium text-xl text-white">Next Todo App</span>
       <div className="flex items-center gap-5">
-        <a href="https://github.com/maricoIR" target="_blank">
+        <a href="https://github.com/maricoIR" target="_blank" rel="noreferrer">
           <LogoGithub
             color={"#fff"}
             width="23px"
@@ -13,7 +13,11 @@ const Navbar = () => {
             cssClasses="cursor-pointer"
           />
         </a>
-        <a href="https://matinkahrizi.vercel.app/" target="_blank">
+        <a
+          href="https://matinkahrizi.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <GlobeOutline
             color={"#fff"}
             width="23px"
